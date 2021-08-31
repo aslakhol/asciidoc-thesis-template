bc55817743efab38ce920aa7dbb64af030642547
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-08-31 18:25:53 +0200

    adds specific script for print

--- hot_reload_print.sh
+++ hot_reload_print.sh
@@ -10,7 +10,7 @@ kill $(lsof -ti tcp:8080)
 python -m http.server 8080 &
 open http://localhost:8080
 fswatch -v ./**/*.adoc index.adoc configuration/* | while IFS= read -r line; do
-     asciidoctor -r asciidoctor-bibtex -a stem -a docinfodir="./configuration/local" index.adoc
+     asciidoctor -r asciidoctor-bibtex -a stem -a docinfodir="./configuration/print" index.adoc
      PID=$!
 
 done
