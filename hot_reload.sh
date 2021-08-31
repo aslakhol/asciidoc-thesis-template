59b87b9bf84bc15aa227cbd1d956e3b360fe7ea5
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-08-31 18:24:49 +0200

    fix quitting in bash-script

--- hot_reload.sh
+++ hot_reload.sh
@@ -1,7 +1,16 @@
+control_c() {
+     kill $(lsof -ti tcp:8080)
+     kill $PID
+     exit
+}
+
+trap control_c SIGINT
+
 kill $(lsof -ti tcp:8080)
 python -m http.server 8080 &
 open http://localhost:8080
-fswatch -v ./**/*.adoc index.adoc docinfo/local/* | while IFS= read -r line;
-do
-     asciidoctor -r asciidoctor-bibtex -a stem -a docinfodir="./configuration/local" index.adoc
+fswatch -v ./**/*.adoc index.adoc configuration/* | while IFS= read -r line; do
+     asciidoctor -r asciidoctor-bibtex -a stem -a docinfodir="./configuration/print" index.adoc
+     PID=$!
+
 done
