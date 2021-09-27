85523ff9fabac23de03a3be597073338d3dc6f06
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-09-27 11:30:06 +0200

    use python 3 instead of python 2 in hot reload script

--- hot_reload_print.sh
+++ hot_reload_print.sh
@@ -7,7 +7,7 @@ control_c() {
 trap control_c SIGINT
 
 kill $(lsof -ti tcp:8080)
-python -m http.server 8080 &
+python3 -m http.server 8080 &
 open http://localhost:8080
 fswatch -v ./**/*.adoc index.adoc configuration/* | while IFS= read -r line; do
      asciidoctor -r asciidoctor-bibtex -a stem -a docinfodir="./configuration/print" index.adoc
